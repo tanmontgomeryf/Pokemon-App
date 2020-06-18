914b1d83feac6bb064f31c52e210736cf1d0875a
Monty Tan <tanmontgomeryf@gmail.com>
2020-06-18 14:51:47 +0800

    prepare to deploy

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 require('dotenv').config();
-
 const express = require('express');
 const cookieParser = require('cookie-parser');
 const connectDB = require('./db');
 const compression = require('compression');
+const path = require('path');
 
 const app = express();
 connectDB();
@@ -15,6 +15,14 @@ app.use(compression());
 app.use('/api/pokemon', require('./routes/api/pokemon'));
 app.use('/api/user', require('./routes/api/user'));
 app.use('/api/auth', require('./routes/api/auth'));
+
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static('client/build'));
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
 
 const PORT = process.env.PORT || 5000;
 
